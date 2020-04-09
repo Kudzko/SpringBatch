526d5356860318236ae78cadc882794ce76a81fa
Alex Kudko <alexkudko@gmail.com>
2020-04-09 23:04:54 +0300

    work someway

--- BatchStartTechPrimers/src/main/java/by/kudko/SpringBatchApplication.java
+++ BatchStartTechPrimers/src/main/java/by/kudko/SpringBatchApplication.java
@@ -22,45 +22,46 @@ public class SpringBatchApplication {
 
     private static final String CLASS_NAME = "Hotel";
     public static void main(String[] args) {
-//        String currentDir = new File("").getAbsolutePath();
-//        File file = new File(currentDir + "\\BatchStartTechPrimers\\src\\main\\java\\by\\kudko\\model\\Hotel.java");
-//        System.out.println(file.getAbsolutePath());
-//
-//
-//        System.out.println();
-//        try {
-//            String str = "" +
-//                    "package by.kudko.model;\n" +
-//                    "\n" +
-//                    "\n" +
-//                    "import lombok.AllArgsConstructor;\n" +
-//                    "import lombok.Data;\n" +
-//                    "import lombok.NoArgsConstructor;\n" +
-//                    "\n" +
-//                    "import javax.persistence.Entity;\n" +
-//                    "\n" +
-//                    "@Data\n" +
-//                    "@NoArgsConstructor\n" +
-//                    "@AllArgsConstructor\n" +
-//                    "@Entity\n" +
-//                    "public class Hotel extends BEntity {\n" +
-//                    "\n" +
-//                    "    private String name;\n" +
-//                    "    private Integer stars;\n" +
-//                    "\n" +
-//                    "    public Hotel(int id, String name, Integer stars) {\n" +
-//                    "        super(id);\n" +
-//                    "        this.name = name;\n" +
-//                    "        this.stars = stars;\n" +
-//                    "    }\n" +
-//                    "}\n" +
-//                    "\n";
-//            byte[] bs = str.getBytes();
-//            Files.write(file.toPath(),bs);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        String currentDir = new File("").getAbsolutePath();
+        System.out.println(currentDir);
+        File file = new File(currentDir + "\\BatchStartTechPrimers\\src\\main\\java\\by\\kudko\\model\\Hotel.java");
+        System.out.println(file.getAbsolutePath());
+
+
+        System.out.println();
+        try {
+            String str = "" +
+                    "package by.kudko.model;\n" +
+                    "\n" +
+                    "\n" +
+                    "import lombok.AllArgsConstructor;\n" +
+                    "import lombok.Data;\n" +
+                    "import lombok.NoArgsConstructor;\n" +
+                    "\n" +
+                    "import javax.persistence.Entity;\n" +
+                    "\n" +
+                    "@Data\n" +
+                    "@NoArgsConstructor\n" +
+                    "@AllArgsConstructor\n" +
+                    "@Entity\n" +
+                    "public class Hotel extends BEntity {\n" +
+                    "\n" +
+                    "    private String name;\n" +
+                    "    private Integer stars;\n" +
+                    "\n" +
+                    "    public Hotel(int id, String name, Integer stars) {\n" +
+                    "        super(id);\n" +
+                    "        this.name = name;\n" +
+                    "        this.stars = stars;\n" +
+                    "    }\n" +
+                    "}\n" +
+                    "\n";
+            byte[] bs = str.getBytes();
+            Files.write(file.toPath(),bs);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try{
 
@@ -100,6 +101,14 @@ public class SpringBatchApplication {
         }catch (Exception e){
 
             e.printStackTrace();
+        }
+
+
+        Runtime rt = Runtime.getRuntime();
+        try {
+            Process pr = rt.exec("javac -cp lombok.jar Hotel.java");
+        } catch (IOException e) {
+            log.warn("Command to create class Flower from xml can not read", e);
         }
 
         SpringApplication.run(SpringBatchApplication.class, args);
